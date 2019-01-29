21bdcf297b583237f5d00cddfbd4116d9cc29a47
GorillaSonglian <xiaopengyoumencaicaiwoshishui@gmail.com>
2019-01-29 14:02:05 +0800

    Add Formatter

---
+++ src/main/java/com/intellij/lang/yang/formatter/YangFormatter.java
@@ -0,0 +1,9 @@
+package com.intellij.lang.yang.formatter;
+
+public class YangFormatter {
+
+    public static void main(String[] args) {
+
+        YangFormatter instance = new YangFormatter();
+    }
+}
