0fca3ca7e87414c614919160c5e79c684a3ec1f6
GorillaSonglian <xiaopengyoumencaicaiwoshishui@gmail.com>
2019-01-29 14:06:08 +0800

    Test

--- src/main/java/com/intellij/lang/yang/formatter/YangFormatter.java
+++ src/main/java/com/intellij/lang/yang/formatter/YangFormatter.java
@@ -1,9 +1,31 @@
 package com.intellij.lang.yang.formatter;
 
-public class YangFormatter {
+import com.intellij.formatting.FormattingModel;
+import com.intellij.formatting.FormattingModelBuilder;
+import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class YangFormatter implements FormattingModelBuilder {
 
     public static void main(String[] args) {
 
         YangFormatter instance = new YangFormatter();
+    }
+
+    @Nullable
+    @Override
+    public TextRange getRangeAffectingIndent(PsiFile file, int offset, ASTNode elementAtOffset) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public FormattingModel createModel(PsiElement element, CodeStyleSettings settings) {
+        return null;
     }
 }
